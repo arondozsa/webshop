b75e4b51e3ff088fdc7fe5f995f5c83b8eb324d3
arondozsa <aron.dozsa@gmail.com>
2022-02-08 13:02:08 +0100

    cart context created

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import './App.css';
 import Header from './Header';
 import { BrowserRouter , Routes, Route, } from 'react-router-dom'
 import Products from './Products';
+import React from 'react'
+import { CartContext } from './CartContext';
 
 function App() {
+  let [cart , setCart] = React.useState(null)
+  console.log(cart)
   return (
     <BrowserRouter>
-    <Header />
-      <Routes>
-        <Route path = '/products' element = {<Products /> } />
+      <CartContext.Provider value =  {{'cart' : cart , 'setCart' : setCart} }>
+        <Header />
+          <Routes>
+            <Route path = '/products/*' element = {<Products /> } />
 
-      </Routes>
+          </Routes>
+
+      </CartContext.Provider>
     </BrowserRouter>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { CartContext } from './CartContext'
 
 export default function Product({name, price, description}) {
-
+  let cartContext = React.useContext(CartContext)
   return (
     <div>
       <h4>{name}</h4>
       <p>{description}</p>
       <p>{price}</p>
+      <button onClick={() => cartContext.cart !== null ? cartContext.setCart([...cartContext.cart, { [name] : price } ]) : cartContext.setCart([{ [name] : price }])}>kosárba</button>
     </div>
   )
 }
